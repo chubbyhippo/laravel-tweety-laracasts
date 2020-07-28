bfad7b9ab47cdf783ae1e587a61f2fa3047b5c3d
ch <mkmsacc@outlook.com>
2020-07-28 21:57:19 +0700

    first commit

---
+++ mysql-init/init.sql
@@ -0,0 +1,13 @@
+-- Adminer 4.7.7 MySQL dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+SET NAMES utf8mb4;
+
+CREATE DATABASE `laravel` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `laravel`;
+
+-- 2020-07-09 14:07:58
