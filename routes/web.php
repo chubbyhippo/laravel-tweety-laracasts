78a4a90be0d43de28d559248a4f02398bc0cf73a
ch <mkmsacc@outlook.com>
2020-08-03 20:16:17 +0700

    add edit profile

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::middleware('auth')->group(function () {
     Route::post('/tweets', 'TweetController@store');
     Route::post('/profiles/{user:username}/follow', 'FollowController@store');
     Route::get('/profiles/{user:username}/edit', 'ProfileController@edit')->middleware('can:edit,user');
+    Route::patch('/profiles/{user:username}', 'ProfileController@update');
 });
 
 Route::get('/profiles/{user:username}', 'ProfileController@show')->name('profile');
