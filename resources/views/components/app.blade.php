a386448dd618b0ff9a29243b21321496b9e6ea40
ch <mkmsacc@outlook.com>
2020-08-05 15:54:48 +0700

    clean up

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -3,7 +3,7 @@
         <main class="container mx-auto">
             <div class="lg:flex lg:justify-between">
                 @auth
-                <div class="lg:w-auto">
+                <div class="lg:w-32">
                     @include('_sidebar-links')
                 </div>
                 @endauth
@@ -11,7 +11,7 @@
                     {{$slot}}
                 </div>
                 @auth
-                <div class="lg:w-1/6 bg-blue-100 rounded-lg p-4">
+                <div class="lg:w-1/6">
                     @include('_friends-list')
                 </div>
                 @endauth
